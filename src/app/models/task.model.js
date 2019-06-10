94c257b3bd88b1e0b526d83ec62b0d8c7647125d
Student <example@example.com>
2019-06-10 13:20:34 -0700

    setup Angular framework

---
+++ src/app/models/task.model.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var Task = /** @class */ (function () {
+    function Task(description) {
+        this.description = description;
+        this.done = false;
+    }
+    return Task;
+}());
+exports.Task = Task;
